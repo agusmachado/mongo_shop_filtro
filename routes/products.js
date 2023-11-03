514fa34175c06b7af3b830c80afee2aaa4bf7b19
agusmachado <agusmachado@gmail.com>
2023-11-03 12:29:47 -0300

    Actualizado el buscador para poder buscar por rangos de precios y botones seteados

--- routes/products.js
+++ routes/products.js
@@ -27,15 +27,12 @@ router.get('/liddy', getAllLiddy)
 // localhost:4600/pepe/ikea
 router.get('/ikea', getAllIkea)
 
-
-
-
 // Ruta para buscar productos por palabra clave
-router.get('/search', async (req, res) => {
+/* router.get('/search', async (req, res) => {
     const keyword = req.query.keyword; // Obtiene la palabra clave ingresada por el usuario desde la URL
     try {
         const products = await Product.find({
-            $or: [
+            $or: [                
                 { name: { $regex: keyword, $options: 'i' } }, // Búsqueda por nombre
                 { company: { $regex: keyword, $options: 'i' } }, // Búsqueda por compañía
                 { price: parseFloat(keyword) || 0 }, // Búsqueda por precio
@@ -45,7 +42,40 @@ router.get('/search', async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: 'Error en el servidor.' });
     }
+});  */
+
+
+
+// Ruta para buscar productos por palabra clave y por precio
+router.get('/search', async (req, res) => {
+    const keyword = req.query.keyword; // Obtiene la palabra clave ingresada por el usuario desde la URL
+    let minPrice = 0; // Precio mínimo por defecto
+    let maxPrice = Number.POSITIVE_INFINITY; // Precio máximo por defecto
+
+    // Verifica si se proporcionan rangos de precios
+    if (req.query.minPrice) {
+        minPrice = parseFloat(req.query.minPrice);
+    }
+    if (req.query.maxPrice) {
+        maxPrice = parseFloat(req.query.maxPrice);
+    }
+
+    try {
+        const products = await Product.find({
+            $or: [                
+                { name: { $regex: keyword, $options: 'i' } },
+                { company: { $regex: keyword, $options: 'i' } },
+            ],
+            price: { $gte: minPrice, $lte: maxPrice } // Búsqueda por precio en un rango
+        });
+        res.render('products', { products });
+    } catch (error) {
+        res.status(500).json({ error: 'Error en el servidor.' });
+    }
 });
+
+
+
 
 
 module.exports = router
